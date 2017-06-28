c3b27d0ec4c4bd6b53ea434c2e77c14ef4bf9d88
Mayesters <maxi@boether.de>
2017-06-28 20:52:34 +0200

    transifex setup

---
+++ transifex.sh
@@ -0,0 +1,20 @@
+mkdir /transifex
+mkdir /transifex/.tx
+cat >/transifex/.tx/config <<EOL
+[main]
+host = https://www.transifex.com
+
+[lbry-app.app-strings]
+file_filter = /transifex/<lang>.json
+source_file = /transifex/en.json
+source_lang = en
+type = KEYVALUEJSON
+minimum_perc = 90
+EOL
+
+cat >~/transifexrc <<EOL
+[https://www.transifex.com]
+hostname = https://www.transifex.com
+password = $1
+username = api
+EOL
